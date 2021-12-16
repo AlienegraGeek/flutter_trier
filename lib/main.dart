25ce7f6abbae48179dad5f25cbce9de4301b266f
AlienegraGeek <13629402950@163.com>
2021-12-16 19:27:43 +0800

    test webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trier/webview_demo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,6 +26,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/webview': (context) => WebViewDemo(),
+      },
     );
   }
 }
@@ -106,7 +110,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        // onPressed: _incrementCounter,
+        onPressed: () {
+          Navigator.pushNamed(context, "/webview");
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
---
+++ lib/webview_demo.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter_plus/webview_flutter_plus.dart';
+
+class WebViewDemo extends StatefulWidget {
+  const WebViewDemo({Key? key}) : super(key: key);
+
+  @override
+  _WebViewDemoState createState() => _WebViewDemoState();
+}
+
+class _WebViewDemoState extends State<WebViewDemo> {
+  late WebViewPlusController _controller;
+  double _height = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ListView Example'),
+      ),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: _height,
+            child: WebViewPlus(
+              onWebViewCreated: (controller) {
+                _controller = controller;
+                controller.loadUrl('assets/index.html');
+              },
+              onPageFinished: (url) {
+                _controller.getHeight().then((double height) {
+                  print("Height:  " + height.toString());
+                  setState(() {
+                    _height = height;
+                  });
+                });
+              },
+              javascriptMode: JavascriptMode.unrestricted,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
